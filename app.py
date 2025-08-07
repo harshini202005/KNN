1aeac6dfb99782b4482537a614a02e2bc559b11e
harshini202005 <harshupranav203@gmail.com>
2025-08-07 10:37:07 +0530

    Files added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -29,4 +30,5 @@ def predict():
         return f"Error: {str(e)}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
